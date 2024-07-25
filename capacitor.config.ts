0ce6f0157293d87e5bc08a50caea8fa7ee628ac0
Muhammad Kharisma Mahardika <muhammadkharismam@gmail.com>
2024-07-25 10:03:38 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nvp-submission',
+  webDir: 'dist'
+};
+
+export default config;
